d58d86211cd69883cba703ae17a728a078e2850f
Gaspard Bucher <gaspard@lucidogen.io>
2018-05-30 22:08:49 +0200

    Added a second tsconfig for build to ignore test files.

--- setup/jest.js
+++ setup/jest.js
@@ -6,9 +6,9 @@ module.exports = {
     '^.+\\.tsx?$': 'ts-jest',
   },
   testRegex: '\\.test\\.tsx?$',
-  testPathIgnorePatterns: ['/lib/', '/node_modules/', '/config/'],
+  testPathIgnorePatterns: ['/lib/', '/node_modules/', '/setup/'],
   // What is here must reflect exactly what is in tsconfig
   moduleNameMapper: {
-    '^test$': '<rootDir>/config/test',
+    '^test$': '<rootDir>/setup/test',
   },
 }
